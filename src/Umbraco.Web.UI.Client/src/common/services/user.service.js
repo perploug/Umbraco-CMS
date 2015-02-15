167d8534ec844413531f1b89e6e3f36301433bef
Per Ploug <ploug@umbraco.dk>
2015-02-15 14:55:12 +0100

    Cleans up user service
    Removes navigation and authentication logic from user service, and tie
    things together with events instead

--- src/Umbraco.Web.UI.Client/src/common/services/user.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/user.service.js
@@ -8,54 +8,25 @@ angular.module('umbraco.services')
         // this is used so that we know when to go and get the user's remaining seconds directly.
         var lastServerTimeoutSet = null;
 
-        function openLoginDialog(isTimedOut) {
-            if (!loginDialog) {
-                loginDialog = dialogService.open({
-
-                    //very special flag which means that global events cannot close this dialog
-                    manualClose: true,
-
-                    template: 'views/common/dialogs/login.html',
-                    modalClass: "login-overlay",
-                    animation: "slide",
-                    show: true,
-                    callback: onLoginDialogClose,
-                    dialogData: {
-                        isTimedOut: isTimedOut
-                    }
-                });
-            }
-        }
-
-        function onLoginDialogClose(success) {
-            loginDialog = null;
-
-            if (success) {
-                securityRetryQueue.retryAll();
-            }
-            else {
-                securityRetryQueue.cancelAll();
-                $location.path('/');
-            }
-        }
-
-        /** 
-        This methods will set the current user when it is resolved and 
-        will then start the counter to count in-memory how many seconds they have 
+        /**
+        This methods will set the current user when it is resolved and
+        will then start the counter to count in-memory how many seconds they have
         remaining on the auth session
         */
-        function setCurrentUser(usr) {
-            if (!usr.remainingAuthSeconds) {
+        function setCurrentUser(data) {
+            if (!data.remainingAuthSeconds) {
                 throw "The user object is invalid, the remainingAuthSeconds is required.";
             }
-            currentUser = usr;
+
+            currentUser = data;
             lastServerTimeoutSet = new Date();
+
             //start the timer
             countdownUserTimeout();
         }
 
-        /** 
-        Method to count down the current user's timeout seconds, 
+        /**
+        Method to count down the current user's timeout seconds,
         this will continually count down their current remaining seconds every 2 seconds until
         there are no more seconds remaining.
         */
@@ -70,8 +41,8 @@ angular.module('umbraco.services')
                     //if there are more than 30 remaining seconds, recurse!
                     if (currentUser.remainingAuthSeconds > 30) {
 
-                        //we need to check when the last time the timeout was set from the server, if 
-                        // it has been more than 30 seconds then we'll manually go and retrieve it from the 
+                        //we need to check when the last time the timeout was set from the server, if
+                        // it has been more than 30 seconds then we'll manually go and retrieve it from the
                         // server - this helps to keep our local countdown in check with the true timeout.
                         if (lastServerTimeoutSet != null) {
                             var now = new Date();
@@ -79,7 +50,7 @@ angular.module('umbraco.services')
 
                             if (seconds > 30) {
 
-                                //first we'll set the lastServerTimeoutSet to null - this is so we don't get back in to this loop while we 
+                                //first we'll set the lastServerTimeoutSet to null - this is so we don't get back in to this loop while we
                                 // wait for a response from the server otherwise we'll be making double/triple/etc... calls while we wait.
                                 lastServerTimeoutSet = null;
 
@@ -98,7 +69,7 @@ angular.module('umbraco.services')
                     }
                     else {
 
-                        //we are either timed out or very close to timing out so we need to show the login dialog.                                        
+                        //we are either timed out or very close to timing out so we need to show the login dialog.
                         if (Umbraco.Sys.ServerVariables.umbracoSettings.keepUserLoggedIn !== true) {
                             //NOTE: the safeApply because our timeout is set to not run digests (performance reasons)
                             angularHelper.safeApply($rootScope, function () {
@@ -109,7 +80,7 @@ angular.module('umbraco.services')
                             //we've got less than 30 seconds remaining so let's check the server
 
                             if (lastServerTimeoutSet != null) {
-                                //first we'll set the lastServerTimeoutSet to null - this is so we don't get back in to this loop while we 
+                                //first we'll set the lastServerTimeoutSet to null - this is so we don't get back in to this loop while we
                                 // wait for a response from the server otherwise we'll be making double/triple/etc... calls while we wait.
                                 lastServerTimeoutSet = null;
 
@@ -134,8 +105,6 @@ angular.module('umbraco.services')
 
         /** Called to update the current user's timeout */
         function setUserTimeoutInternal(newTimeout) {
-
-
             var asNumber = parseFloat(newTimeout);
             if (!isNaN(asNumber) && currentUser && angular.isNumber(asNumber)) {
                 currentUser.remainingAuthSeconds = newTimeout;
@@ -158,24 +127,10 @@ angular.module('umbraco.services')
             currentUser = null;
 
             //broadcast a global event that the user is no longer logged in
-            eventsService.emit("app.notAuthenticated");
-
-            openLoginDialog(isLogout === undefined ? true : !isLogout);
+            eventsService.emit("app.authenticationRequired", {isLogOut: isLogout});
         }
 
-        // Register a handler for when an item is added to the retry queue
-        securityRetryQueue.onItemAddedCallbacks.push(function (retryItem) {
-            if (securityRetryQueue.hasMore()) {
-                userAuthExpired();
-            }
-        });
-
         return {
-
-            /** Internal method to display the login dialog */
-            _showLoginDialog: function () {
-                openLoginDialog();
-            },
 
             /** Returns a promise, sends a request to the server to check if the current cookie is authorized  */
             isAuthenticated: function () {
@@ -185,7 +140,20 @@ angular.module('umbraco.services')
                     deferred.resolve(true);
                     return deferred.promise;
                 }
+
                 return authResource.isAuthenticated();
+            },
+
+            hasSectionAccess: function(section){
+                if(!currentUser){
+                    return false;
+                }
+
+                if(currentUser.allowedSections.indexOf(section) < 0){
+                    return false;
+                }
+
+                return true;
             },
 
             /** Returns a promise, sends a request to the server to validate the credentials  */
@@ -200,18 +168,17 @@ angular.module('umbraco.services')
                         var result = { user: data, authenticated: true, lastUserId: lastUserId };
 
                         //broadcast a global event
-                        eventsService.emit("app.authenticated", result);
+                        eventsService.emit("app.authenticationConfirmed", result);
                         return result;
                     });
             },
 
-            /** Logs the user out 
+            /** Logs the user out
              */
             logout: function () {
-
                 return authResource.performLogout()
                     .then(function(data) {
-                        userAuthExpired();
+                        userAuthExpired(true);
                         //done!
                         return null;
                     });
@@ -225,28 +192,13 @@ angular.module('umbraco.services')
                     authResource.getCurrentUser()
                         .then(function (data) {
 
-                            var result = { user: data, authenticated: true, lastUserId: lastUserId };
+                            var result = data;
+                            result.authenticated = true;
+                            result.lastUserId = lastUserId;
 
-                            //TODO: This is a mega backwards compatibility hack... These variables SHOULD NOT exist in the server variables
-                            // since they are not supposed to be dynamic but I accidentally added them there in 7.1.5 IIRC so some people might
-                            // now be relying on this :(
-                            if (Umbraco && Umbraco.Sys && Umbraco.Sys.ServerVariables) {
-                                Umbraco.Sys.ServerVariables["security"] = {
-                                    startContentId: data.startContentId,
-                                    startMediaId: data.startMediaId
-                                };
-                            }
-
-                            if (args && args.broadcastEvent) {
-                                //broadcast a global event, will inform listening controllers to load in the user specific data
-                                eventsService.emit("app.authenticated", result);
-                            }
-
-                            setCurrentUser(data);
-                            currentUser.avatar = '//www.gravatar.com/avatar/' + data.emailHash + '?s=40&d=404';
+                            setCurrentUser(result);
                             deferred.resolve(currentUser);
-                        });
-
+                    });
                 }
                 else {
                     deferred.resolve(currentUser);
