621a063af245ec0adfec64359e6ec452af94ac7d
Per Ploug <ploug@umbraco.dk>
2015-02-15 15:11:48 +0100

    Updates Folderbrowser to user angular-upload
    the blueimp upload is not compatible with angular 1.3 and therefore
    needs to be replaced with angular-upload which is actually slimmer,
    faster and easier to work with

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/folderbrowser/folderbrowser.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/folderbrowser/folderbrowser.controller.js
@@ -1,82 +1,75 @@
 angular.module("umbraco")
-.directive("umbUploadPreview",function($parse){
-        return {
-            link: function(scope, element, attr, ctrl) {
-               var fn = $parse(attr.umbUploadPreview),
-                                   file = fn(scope);
-                if (file.preview) {
-                    element.append(file.preview);
-               }
+.controller("Umbraco.PropertyEditors.FolderBrowserController",
+    function ($rootScope, $scope, $upload, $timeout, $routeParams, mediaResource, umbRequestHelper) {
+        
+        $scope.$watch('files', function () {
+            $scope.upload($scope.files);
+        });
+
+        function readmultifiles(files) {
+            
+            var reader = new FileReader();  
+            function readFile(index) {
+                if( index >= files.length ) return;
+
+                var file = files[index];
+                reader.onload = function(e) {  
+                    // get file content  
+                    var bin = e.target.result; 
+
+                    // do sth with bin
+                    $scope.$apply(function(){
+                        files[index].inBrowserSrc = bin;
+                        readFile(index+1)
+                    });
+                }
+
+                reader.readAsDataURL(file);
+            }
+            readFile(0);
+        }
+
+
+        $scope.upload = function (files) {
+            if (files && files.length) {
+                
+                readmultifiles(files);
+
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    file.progress = 0;                    
+                    $upload.upload({
+                        url: umbRequestHelper.getApiUrl("mediaApiBaseUrl", "PostAddFile"),
+                        fields: {'currentFolder': $routeParams.id},
+                        file: file
+                    }).progress(function (evt) {
+                        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                        evt.config.file.progress = progressPercentage;
+                    }).success(function (data, status, headers, config) {
+
+                        var index = files.indexOf(config.file);
+                        if(index >= 0){
+                              ///  files.splice(index,1);
+                            
+                            if(files.length === 0){
+                               // $scope.loadChildren($routeParams.id);
+                            }
+                        }
+                        
+                    });
+                }
             }
         };
-})
-.controller("Umbraco.PropertyEditors.FolderBrowserController",
-    function ($rootScope, $scope, assetsService, $routeParams, $timeout, $element, $location, $log, umbRequestHelper, mediaResource, imageHelper, navigationService, editorState) {
-        var dialogOptions = $scope.dialogOptions;
 
-        $scope.creating = $routeParams.create;
+        
+        $scope.loadChildren = function(id){
+            mediaResource.getChildren(id)
+                .then(function(data) {
+                    $scope.images = data.items;
+                });    
+        };
 
-        if(!$scope.creating){
-
-            $scope.filesUploading = [];
-            $scope.options = {                
-                url: umbRequestHelper.getApiUrl("mediaApiBaseUrl", "PostAddFile"),
-                autoUpload: true,
-                disableImageResize: /Android(?!.*Chrome)|Opera/
-                .test(window.navigator.userAgent),
-                previewMaxWidth: 200,
-                previewMaxHeight: 200,
-                previewCrop: true,
-                formData:{
-                    currentFolder: $routeParams.id
-                }
-            };
-
-
-            $scope.loadChildren = function(id){
-                mediaResource.getChildren(id)
-                    .then(function(data) {
-                        $scope.images = data.items;
-                    });    
-            };
-
-            $scope.$on('fileuploadstop', function(event, files){
-                $scope.loadChildren($scope.options.formData.currentFolder);
-                
-                //sync the tree - don't force reload since we're not updating this particular node (i.e. its name or anything),
-                // then we'll get the resulting tree node which we can then use to reload it's children.
-                var path = editorState.current.path;
-                navigationService.syncTree({ tree: "media", path: path, forceReload: false }).then(function (syncArgs) {
-                    navigationService.reloadNode(syncArgs.node);
-                });
-
-                $scope.queue = [];
-                $scope.filesUploading = [];
-            });
-
-            $scope.$on('fileuploadprocessalways', function(e,data) {
-                var i;
-                $scope.$apply(function() {
-                    $scope.filesUploading.push(data.files[data.index]);
-                });
-            });
-
-            // All these sit-ups are to add dropzone area and make sure it gets removed if dragging is aborted! 
-            $scope.$on('fileuploaddragover', function(event, files) {
-                if (!$scope.dragClearTimeout) {
-                    $scope.$apply(function() {
-                        $scope.dropping = true;
-                    });
-                } else {
-                    $timeout.cancel($scope.dragClearTimeout);
-                }
-                $scope.dragClearTimeout = $timeout(function () {
-                    $scope.dropping = null;
-                    $scope.dragClearTimeout = null;
-                }, 300);
-            });
-            
-            //init load
-            $scope.loadChildren($routeParams.id);
-        }
+        //init load
+        $scope.loadChildren($routeParams.id);
+        
 });
